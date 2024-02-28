b05289a982b7f2f1436aae43c16a7e0767096135
GageHoechst <hoechstg99@gmail.com>
2024-02-28 11:52:27 -0500

    one product on the server

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,12 @@
 class ProductsController < ApplicationController
   def one_product
-    @Product = Product.find_by
-    (id: params{:id})
-    render :show
-    
+    product = Product.first
+    render json: {
+      name: product.name,
+      price: product.price,
+      image_url: product.image_url,
+      description: product.description,
+
+    }
   end
 end
