c216220f0b0e1c43c5ac284b05c2ed9380b5ea74
GageHoechst <hoechstg99@gmail.com>
2024-03-07 10:56:45 -0500

    added supplier id to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_06_182518) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_07_155210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_06_182518) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "image_id"
+    t.integer "supplier_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
